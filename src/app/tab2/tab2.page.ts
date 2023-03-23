9ee60d7b5695ab7498543681d2c637054eeee720
HazzaZardo <zardoharry@gmail.com>
2023-03-23 12:05:44 -0300

    atualizou

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  valendo = 1;
+  pontuacaoTime1 = 0;
+  pontuacaoTime2 = 0;
+  partidasTime1 = 0;
+  partidasTime2 = 0;
 
   constructor() {}
+
+  mudarValendo3() {
+    this.valendo = 3;
+  }
+
+  mudarValendo6() {
+    this.valendo = 6;
+  }
+
+  mudarValendo9() {
+    this.valendo = 9;
+  }
+
+  mudarValendo12() {
+    this.valendo = 12;
+  }
+
+  mudarPontuacaoTime1Mais() {
+    
+  }
 
 }
